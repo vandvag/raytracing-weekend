84925cc71ccc4fb8c64ae1300da8759c85b73dfc
Evangelos Vandoros <vandvag@gmail.com>
2025-10-04 22:35:03 +0300

    Made vector class significantly more simple

--- src/vec.zig
+++ src/vec.zig
@@ -1,64 +1,49 @@
-pub inline fn vsize(comptime T: type) comptime_int {
-    _ = ensureVector(T);
+const std = @import("std");
 
-    return @typeInfo(T).vector.len;
+pub const Vec3 = @Vector(3, f64);
+
+pub const zero = Vec3{ 0, 0, 0 };
+pub const one = Vec3{ 1, 1, 1 };
+
+pub fn x(v: Vec3) f64 {
+    return v[0];
 }
 
-pub inline fn vtype(comptime T: type) type {
-    _ = ensureVector(T);
-
-    return @typeInfo(T).vector.child;
+pub fn y(v: Vec3) f64 {
+    return v[1];
 }
 
-pub inline fn len2(v: anytype) vtype(@TypeOf(v)) {
-    _ = ensureVector(v);
+pub fn z(v: Vec3) f64 {
+    return v[2];
+}
+
+pub inline fn len2(v: Vec3) f64 {
     return dot(v, v);
 }
 
-pub inline fn len(v: anytype) vtype(@TypeOf(v)) {
-    return @sqrt(len2(v, v));
+pub inline fn len(v: Vec3) f64 {
+    return std.math.sqrt(len2(v));
 }
 
-pub inline fn dot(v1: anytype, v2: anytype) vtype(@TypeOf(v1)) {
-    const vt1 = ensureVector(v1);
-    const vt2 = ensureVector(v2);
-
-    if (vt1 != vt2) {
-        @compileError("Vectors must be of the same type");
-    }
-
+pub inline fn dot(v1: Vec3, v2: Vec3) f64 {
     return @reduce(.Add, v1 * v2);
 }
 
-pub inline fn cross3(v1: anytype, v2: anytype) @TypeOf(v1) {
-    const vt1 = ensureVector(v1);
-    const vt2 = ensureVector(v2);
-
-    if (vt1 != vt2) {
-        @compileError("Vectors must be of the same type");
-    }
-
-    if (vsize(vt1) != 3) {
-        @compileError("Cross product is only defined for 3D vectors");
-    }
-
-    const x = v1[1] * v2[2] - v1[2] * v2[1];
-    const y = v1[2] * v2[0] - v1[0] * v2[2];
-    const z = v1[0] * v2[1] - v1[1] * v2[0];
-
-    return vt1{.{ x, y, z }};
+pub inline fn cross3(v1: Vec3, v2: Vec3) Vec3 {
+    return .{
+        v1[1] * v2[2] - v1[2] * v2[1],
+        v1[2] * v2[0] - v1[0] * v2[2],
+        v1[0] * v2[1] - v1[1] * v2[0],
+    };
 }
 
-pub inline fn unit(v: anytype) @TypeOf(v) {
-    const vt = ensureVector(v);
+pub inline fn unit(v: Vec3) Vec3 {
+    const length = len(v);
 
-    return v / @as(vt, @splat(len(v)));
-}
-
-inline fn ensureVector(comptime T: type) type {
-    if (@typeInfo(T) != .Vector) {
-        @compileError("Expected a vector type, got: " ++ @typeName(T));
+    if (length == 0) {
+        return zero;
     }
 
-    return T;
+    const splatted: Vec3 = @splat(length);
+    return v / splatted;
 }
