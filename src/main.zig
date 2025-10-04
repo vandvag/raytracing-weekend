392e7633cf292c3237e637db42e8512dd6236f02
Evangelos Vandoros <vandvag@gmail.com>
2025-10-04 21:32:22 +0300

    Added progress bar

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Progress = std.Progress;
 
 const IMG_WIDTH = 256;
 const IMG_HEIGHT = 256;
@@ -8,9 +9,18 @@ pub fn main() !void {
     var file_writer = std.fs.File.stdout().writer(&wbuffer);
     const out = &file_writer.interface;
 
+    var progress_buffer: [1024]u8 = undefined;
+    const progress = Progress.start(.{
+        .draw_buffer = &progress_buffer,
+        .estimated_total_items = IMG_HEIGHT * IMG_WIDTH,
+        .root_name = "Rendering",
+    });
+    defer progress.end();
+
     try out.print("P3\n{d} {d}\n255\n", .{ IMG_WIDTH, IMG_HEIGHT });
 
     for (0..IMG_HEIGHT) |h| {
+        progress.completeOne();
         for (0..IMG_WIDTH) |w| {
             const h_float: f32 = @floatFromInt(h);
             const w_float: f32 = @floatFromInt(w);
